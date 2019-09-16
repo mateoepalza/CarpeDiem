c40e633df0273945b3c9a2a34dd023e1b68bcca7
Mateo <mateoepalzaramirez@gmail.com>
2019-09-15 21:10:11 -0500

    i added the slider

--- js/index2.js
+++ js/index2.js
@@ -7,5 +7,41 @@ $(document).ready(function() {
 	});
 
 	//methods
+
 	$.fn.fullpage.setAllowScrolling(true);
+
+	slide();
+
 });
+
+function slide (){
+
+	var i = 1;
+	$(".menu-slide input").click(function(){
+		$(".menu-slide input:nth-child("+(i)+")").removeAttr("checked");
+		i = this.value;
+		$("#slide").css("backgroundImage","url('Slider/1/"+i+".jpg')");
+		$(".menu-slide input:nth-child("+i+")").prop('checked', true);
+	});
+
+
+	setInterval(function(){
+
+		if(i>=5){
+			$(".menu-slide input:nth-child("+(i)+")").removeAttr("checked");
+			i=1;
+
+			$("#slide").css("backgroundImage","url('Slider/1/"+i+".jpg')");
+
+			$(".menu-slide input:nth-child("+i+")").prop('checked', true);
+		}else{
+			i++;
+				$("#slide").css("backgroundImage","url('Slider/1/"+i+".jpg')");
+				$(".menu-slide input:nth-child("+(i-1)+")").removeAttr("checked");
+				$(".menu-slide input:nth-child("+i+")").prop('checked', true);
+		}
+
+
+	},5000);
+
+}
